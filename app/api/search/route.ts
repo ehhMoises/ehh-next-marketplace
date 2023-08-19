915ec11b34eb4e27ac0de703e5fc4d7aa016a4c3
Moises Aburto <moises.aburto@email.com>
2023-08-18 18:04:20 -0600

    Removing out no needed code

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import * as cookie from 'cookie';
 
-export const getPageCookies = (cookieInput: string | undefined) => cookie.parse(cookieInput ?? '');
 export async function POST(req: Request) {
   const accessTokenRecord = cookie.parse(req.headers.get('cookie') ?? '');
   console.log('accessToken', accessTokenRecord['access-token']);
